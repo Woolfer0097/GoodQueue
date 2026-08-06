d6348825a8120bb5a0fa2a1f7d7a9b124ed03510
Ivan <z.ivan1220@gmail.com>
2026-08-06 23:40:17 +0300

    chore(frontend): set up mantine, postcss and app theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import '@mantine/core/styles.css';
 
-import App from './App.tsx'
+import { MantineProvider } from '@mantine/core';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+import { theme } from './theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
